1db6a11e31eb98a4f516de1a1411a019b829b3bb
Valeriu Tihai <valeriu@tihai.md>
2016-05-27 16:14:31 -0400

    FIX: fixed #1

--- uninstall.php
+++ uninstall.php
@@ -4,17 +4,9 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
-// delete the option from the options table
+// Delete the option from the options table
 if (is_multisite()) {
-	global $wpdb;
-	$blogs = $wpdb->get_results("SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A);
-
-	if(!empty($blogs)) {
-		foreach($blogs as $blog) {
-			switch_to_blog($blog['blog_id']);
-			delete_option('analyticstracker_settings');
-		}
-	}
+	delete_site_option('analyticstracker_settings');
 } else {
 	delete_option('analyticstracker_settings');
 }
